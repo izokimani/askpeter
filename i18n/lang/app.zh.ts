fd1aff7acb9ed4c2a4b305870b126f006b11f2f7
Izo Kimani <56126464+izokimani@users.noreply.github.com>
2024-02-27 08:11:45 +0700

    Update app.zh.ts

--- i18n/lang/app.zh.ts
+++ i18n/lang/app.zh.ts
@@ -1,28 +1,28 @@
 const translation = {
   common: {
-    welcome: '欢迎使用',
-    appUnavailable: '应用不可用',
-    appUnkonwError: '应用不可用',
+    welcome: 'Welcome',
+    appUnavailable: 'App Unavailable',
+    appUnknownError: 'Unknown Application Error',
   },
   chat: {
-    newChat: '新对话',
-    newChatDefaultName: '新的对话',
-    openingStatementTitle: '对话开场白',
+    newChat: 'Ask Peter',
+    newChatDefaultName: 'Ask Peter',
+    openingStatementTitle: 'Conversation Opener',
     powerBy: 'Powered by',
-    prompt: '提示词',
-    privatePromptConfigTitle: '对话设置',
-    publicPromptConfigTitle: '对话前提示词',
-    configStatusDes: '开始前，您可以修改对话设置',
-    configDisabled: '此次会话已使用上次会话表单',
-    startChat: '开始对话',
-    privacyPolicyLeft: '请阅读由该应用开发者提供的',
-    privacyPolicyMiddle: '隐私政策',
-    privacyPolicyRight: '。',
+    prompt: 'Cue',
+    privatePromptConfigTitle: 'Conversational Settings',
+    publicPromptConfigTitle: 'Pre-Conversation Prompt',
+    configStatusDes: 'Before starting, you can modify the conversational settings',
+    configDisabled: 'This session will use the previous session’s form',
+    startChat: 'Start Conversation',
+    privacyPolicyLeft: 'Please read the ',
+    privacyPolicyMiddle: 'Privacy Policy',
+    privacyPolicyRight: ' provided by the app developer.',
   },
   errorMessage: {
-    valueOfVarRequired: '变量值必填',
-    waitForResponse: '请等待上条信息响应完成',
+    valueOfVarRequired: 'The value of the variable is required',
+    waitForResponse: 'Please wait for the previous message to finish processing',
   },
 }
 
-export default translation
+export default translation;
